ab932d9056c83f292b4b0df506a423ef58a60405
yandeu <20306025+yandeu@users.noreply.github.com>
2022-04-13 16:02:46 +0200

    .

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tarif590 to JSON
+
+Turns `tarif590_2021_01_01_final.xlsx` into `tarif590.json`.
---
+++ index.mjs
@@ -0,0 +1,33 @@
+import { read } from 'xlsx/xlsx.mjs'
+import { resolve } from 'path'
+import { readFile, writeFile } from 'fs/promises'
+
+const buffer = await readFile(resolve('tarif590_2021_01_01_final.xlsx'))
+const workbook = read(buffer, {
+  sheetStubs: true,
+  cellDates: true,
+  cellStyles: true
+})
+
+const sheet = workbook.Sheets[workbook.SheetNames[0]]
+let tarife = {}
+const now = new Date()
+
+for (let i = 3; i < 500; i++) {
+  // needs to have a valid "Abrechnungsziffer"
+  const ziffer = +sheet[`M${i}`]?.v
+  if (!ziffer) continue
+
+  // needs to be "Valid" by date
+  const validUntil = sheet[`U${i}`]?.v
+  if (new Date(validUntil) < new Date()) continue
+
+  const position = sheet[`M${i}`].v
+  const de = sheet[`N${i}`].v
+  const fr = sheet[`O${i}`].v
+  const it = sheet[`P${i}`].v
+
+  if (position && de && fr && it) tarife = { ...tarife, [position]: { de, fr, it } }
+}
+
+await writeFile(resolve('tarif590.json'), JSON.stringify(tarife), { encoding: 'utf-8' })
